e748a5c7f10a7fed5995221672d02476409ab92f
Praveen Rohra <rohrapraveen79@gmail.com>
2026-06-11 18:20:15 +0530

    Fix Supabase environment variable

--- src/db/supabase.js
+++ src/db/supabase.js
@@ -1,7 +1,11 @@
-import {createClient} from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseKey
+);
 
 export default supabase;
